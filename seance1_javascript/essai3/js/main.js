b40c50aa682236beda237d904535f46d7fc33d2b
Patrick Croquet <patrick.croquet@wanadoo.fr>
2020-11-13 11:46:39 +0100

    séance 1

--- seance1_javascript/essai3/js/main.js
+++ seance1_javascript/essai3/js/main.js
@@ -1,18 +1,17 @@
-let drawer = parseInt(prompt('Choisissez le tiroir à ouvrir (1 à 4) :'));
-
+let drawer = parseInt(prompt("Choisissez le tiroir à ouvrir (1 à 4) :"));
 switch (drawer) {
-    case 1:
-        alert("contient divers outils pour dessiner : du papier, des crayons, etc");
-        break;
-    case 2:
-        alert("contient du matériel informatique");
-        break;
-    case 3:
-        alert("ah ? Ce tiroir est fermé à clé !");
-        break;
-    case 4:
-        alert("Contient des vêtement");
-        break;
-    default:
-        alert("le meuble ne contients que 4 tiroirs");                 
+case 1:
+ alert("Contient divers outils pour dessiner : du papier, des crayons, etc.");
+ break;
+case 2:
+ alert("Contient du matériel informatique : des câbles, des composants, etc.");
+ break;
+case 3:
+ alert("Ah ? Ce tiroir est fermé à clé ! Dommage !");
+ break;
+case 4:
+ alert("Contient des vêtements : des chemises, des pantalons, etc.");
+ break;
+default:
+ alert("Info du jour : le meuble ne contient que 4 tiroirs et, jusqu’à preuve du contraire, les tiroirs négatifs n’existent pas.");
 }
