4ab2ba3d8119677ed9128b1f9bfbb771ee7dc4e1
Patrick Croquet <patrick.croquet@wanadoo.fr>
2020-11-18 11:46:16 +0100

    séance 3

---
+++ seance3_javascript/essai1/js/main.js
@@ -0,0 +1,15 @@
+// let myArray = ['Sébastien','Laurence','Ludovic','Pauline','Guillaume'];
+
+// let PersonArray = [
+//     {
+//         nick: 'Sébastien',
+//         age: 25,
+//         sex: 'm',
+//         parent: 'aîné',
+//         work: 'Développeur'
+//     },{
+//         nick: 'Laurence',
+//         ...
+//     },
+//     // et ainsi de suite...
+// ]; 
---
+++ seance3_javascript/essai2/js/main.js
@@ -0,0 +1,68 @@
+// Définition de l’objet Person via un constructeur
+function Person(nick, age, sex, parent, work, friends) {
+    this.nick = nick;
+    this.age = age;
+    this.sex = sex;
+    this.parent = parent; 
+    this.work = work;
+    this.friends = friends;
+
+    this.addFriend = function(nick, age, sex, parent, work, friends)
+    {
+        this.friends.push(new Person(nick, age, sex, parent, work, friends));  
+    };
+};
+// On crée des variables qui vont contenir une instance de l’objet Person :
+let seb = new Person('Sébastien', 25,'m','aîné','développeur',[]);
+let lau = new Person('Laurence', 19 ,'f','soeur','Sous-officier',[]);
+
+alert(seb.nick + ' et ' + lau.nick); // Affiche : « Sébastien » 
+alert(lau.nick); // Affiche : « Laurence »
+
+seb.nick = 'Bastien';
+alert(seb.nick);
+
+
+
+seb.friends.push(new Person('Johnatan','19','m','aîné','Développeur',[]));
+seb.friends.push(new Person('Eric','20','m','aîné','Développeur',[]));
+seb.friends.push(new Person('Thomas','20','m','aîné','Développeur',[]));
+alert(seb.friends[2].nick);
+
+seb.addFriend('William','19','m','aîné','Développeur',[]);
+alert(seb.friends[3].nick + ' ' + seb.friends[3].age);
+
+
+let fruitArray = [];
+fruitArray.push('Orange');
+
+// On définit un objet Chien
+function Chien(name, color, age, size){
+    this.name = name;
+    this.color = color;
+    this.age = age;
+    this.size = size;
+
+    // this.AddSize = function(size){
+    //     this.size.push(size);
+    // };
+}
+
+Chien.prototype.AddSize = function(size){
+        this.size.push(size);
+    };
+
+// On crée des variables qui vont contenir une instance de l’objet Chien :
+
+let labrador = new Chien('Wil','sable',3,[]);
+let patou = new Chien('Ben','blanc',2,[]);
+
+alert(labrador.name + ' ' + patou.name);
+
+labrador.AddSize('15'); //première visite
+labrador.AddSize('17'); //deuxième visite
+labrador.AddSize('18'); //Troisième visite
+
+labrador.size[0] = '14';
+labrador.age = '4';
+alert(labrador.size[0]); // Affiche 14
