57a96e0ea8e96a6e0b261c9c5a8975a5a1831b0c
Patrick Croquet <patrick.croquet@wanadoo.fr>
2020-11-23 10:21:21 +0100

    Solutions exercices séance 3

--- seance3_javascript/exercices3_javascript/exo3/js/main.js
+++ seance3_javascript/exercices3_javascript/exo3/js/main.js
@@ -1,17 +1,17 @@
 // Ecrire une fonction Javascript qui génère toutes les combinaisons d'une chaîne de caractères.  
 function substrings(str1)  
 {  
-var array1 = [];  
+let array1 = [];  
   for (var x = 0, y = 1; x < str1.length; x++,y++)   
   {  
    array1[x]=str1.substring(x, y); 
     }
    //alert(array1 + ' - ' + array1.length); //Affiche c,h,a,t - 4 
-var combi = [];  
-var temp= "";  
-var slent = Math.pow(2, array1.length);  
+let combi = [];  
+let temp= "";  
+let slent = Math.pow(2, array1.length);  //Calcul du nombre de combinaisons // 16 combinaisons possibles - la combinaison chaîne vide.
   
-for (var i = 0; i < slent ; i++)  
+for (let i = 0; i < slent ; i++)  
 {  
     temp= "";  
     for (var j = 0; j < array1.length ; j++) {  
@@ -19,7 +19,7 @@ for (var i = 0; i < slent ; i++)
             temp += array1[j];  
         }  
     }  
-    if (temp !== "")  
+    if (temp !== "")  //Une combinaison si ce n'est pas la chaîne vide.
     {  
         combi.push(temp);  
     }  
--- seance3_javascript/exercices3_javascript/exo6/js/main.js
+++ seance3_javascript/exercices3_javascript/exo6/js/main.js
@@ -3,10 +3,10 @@ function find_longest_word(str)
   const regex = /\w[a-z\u00E0-\u00FC]{0,}/gi; // récupère les mots dans str
   //const regex = /[A-Z]/gi; // récupère les lettres
   let array1 = str.match(regex);
-  let result = array1[0];
-  //alert(array1);
+  let result = array1[0]; //Tutorial
+  //alert(array1.length); // Affiche 3
 
-  for(let x = 0 ; x < array1.length ; x++)
+  for(let x = 0 ; x < array1.length ; x++) 
   {
     if(result.length < array1[x].length)
     {
