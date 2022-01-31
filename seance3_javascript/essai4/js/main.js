b825465af26d23ee2f6f9bd08eb558347a057f77
Patrick-Croquet <58164643+Patrick-Croquet@users.noreply.github.com>
2022-01-31 16:02:56 +0100

    Initial commit

---
+++ seance3_javascript/essai4/js/main.js
@@ -0,0 +1,36 @@
+// let myString = "C'est un test";
+// let myArray = myString.split(' ');
+
+// length = myArray.length;
+
+// for(var i=0;i<length; i++){ 
+//     //console.log('Index : ' + i+ '\n' +'Valeur : ' + myArray[i]);
+//     console.log('\n' + myArray[i]);
+// }
+
+
+// let myArray = ["C’est", "un", "test"];
+// myArray.forEach(function(value, index) { 
+//     //console.log('Index : ' + index + '\n' +'Valeur : ' + value); 
+//     console.log('\n' + value);
+//     });
+
+// let myArray = ["C’est", "un", "test"];
+// myArray.forEach(element => console.log('\n' + element));
+
+let animalsArray = ["cat", "dog", "fish"];
+for (let value of animalsArray){
+    console.log(value);
+}
+
+// let Person = {
+//         nick: 'Sébastien',
+//         age: 25,
+//         sex: 'm',
+//         parent: 'aîné',
+//         work: 'Développeur'
+//     };
+ 
+// for (let property in Person) {
+//     console.log(`${property} => ${Person[property]}`);
+// }
