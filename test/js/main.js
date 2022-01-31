6cf4c164a422541947fb0213778710b9dfc92a80
Patrick-Croquet <58164643+Patrick-Croquet@users.noreply.github.com>
2022-01-31 16:18:52 +0100

    Initial commit

---
+++ test/js/main.js
@@ -0,0 +1,24 @@
+/* let number = 0;
+number = number + 1;
+alert(number);
+
+number--;
+alert(number); */
+
+// let number_1 = 0; 
+// let number_2 = 0;
+
+// number_1++; 
+// ++number_2;
+// alert(number_1); // Affiche : « 1 » 
+// alert(number_2); // Affiche : « 1 »
+
+let number = 0;
+let output = number++;
+alert(number); //affiche 1
+alert(ouput); //affiche 0
+
+let number = 0;
+let output = ++number;
+alert(number); //affiche 1
+alert(ouput); //affiche 1
