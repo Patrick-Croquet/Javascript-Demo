d4aace2a5d29417930ea2e2933281a47efc02dd7
Patrick-Croquet <58164643+Patrick-Croquet@users.noreply.github.com>
2022-10-24 14:50:06 +0200

    Initial commit

--- seance2_javascript/essai5a/js/main.js
+++ seance2_javascript/essai5a/js/main.js
@@ -1,19 +1,24 @@
 let element = document.getElementById('clickme');
 
 element.style.borderRadius = "10px";
-element.style.backgroundColor = "#FFFF00";
+//element.style.backgroundColor = "#FFFF00";
 
 element.addEventListener('click', function(){
     //alert('Vous avez cliqué !');
-    element.style.backgroundColor = "#00FF00";
+    this.classList.add("bgCred");
     },false);
 
 element.addEventListener('mouseover', function(){
     //alert('Vous avez cliqué !');
-    element.style.backgroundColor = "#FF0000";
+    //element.style.backgroundColor = "#FF0000";
+    this.classList.add("bgCgreen");
     },false);
 
 element.addEventListener('mouseout', function(){
     //alert('Vous avez cliqué !');
-    element.style.backgroundColor = "#FFFF00";
+    //element.style.backgroundColor = "#FFFF00";
+    this.classList.remove("bgCgreen");
+    if (this.classList.contains('bgCred')) {
+        this.classList.remove("bgCred");
+        }
     },false);
